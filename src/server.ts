9447f5550b48a3c85f8e6e6e56028226d4e1a018
santospedroh <santos.pedroh@gmail.com>
2021-06-22 17:25:06 -0300

    Instacao typeoms, criacao de migration

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,9 @@
+import "reflect-metadata";
 import express from "express";
+import "./database";
 
 // @types/express
 const app = express();
-
-/**
- * Metodos HTTP
- * 
- * GET   => Buscar uma informação
- * POST  => Inserir (criar) uma informação
- * PUT   => Alterar uma informação
- * DELET => Deletar uma informação
- * PATCH => Alterar uma informação específica
- * 
- */
-
-app.get("/test", (request, response) => {
-    // Resquet   => Entrando
-    // Respoonse => Saindo
-    return response.send("Olá NLW");
-});
-
-app.post("/test-post", (request, response) => {
-    return response.send("Olá Post NLW");
-});
 
 //http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
